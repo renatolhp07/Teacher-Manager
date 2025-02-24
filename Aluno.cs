d7bead6a894f96a2f5576a387b685af2683aba46
Renato Vieira Santos <renato.vieiralhp@gmail.com>
2025-02-24 00:45:37 -0300

    Adicionando classe Aluno

---
+++ Aluno.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teacher_Manager
+{
+    internal class Aluno
+    {
+        private int id_aluno;
+        private string nome_aluno;
+        private string cpf_aluno;
+
+    }
+}
